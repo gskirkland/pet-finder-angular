0a6ce81c2142af03262cf101c440455e77e79af2
Gerald Scott Kirkland <skirkland8@gmail.com>
2019-08-22 19:56:51 -0400

    Added ngFor directive to select boxes on search form. Added arrays for select box values.

--- src/app/components/pet-search/pet-search.component.ts
+++ src/app/components/pet-search/pet-search.component.ts
@@ -11,6 +11,18 @@ import { Pet } from '../../models/Pet';
 export class PetSearchComponent implements OnInit {
   pets: Pet[];
 
+  // petType select box array
+  petTypes = ['Cat', 'Dog', 'Other'];
+
+  // searchDistance select box array
+  searchDistances = ['25', '50', '100'];
+
+  // dateRange select box array
+  dateRanges = ['Last 3 Days', 'Last Week', 'Last 2 Weeks', 'Last Month', 'Any Time'];
+
+  // sortBy select box array
+  sortBySelections = ['Most Recent', 'Distance'];
+
   constructor(private petService: PetService) { }
 
   ngOnInit() {
