b4e257e7d1ea9c26b225cd049b916b1f31422f6b
Gerald Scott Kirkland <skirkland8@gmail.com>
2019-08-25 14:16:57 -0400

    Building reactiveForm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -24,7 +25,8 @@ import { ReportPetComponent } from './components/report-pet/report-pet.component
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/pet-search/pet-search.component.ts
+++ src/app/components/pet-search/pet-search.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PetService } from '../../services/pet.service';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 import { Pet } from '../../models/Pet';
 
@@ -9,60 +11,86 @@ import { Pet } from '../../models/Pet';
   styleUrls: ['./pet-search.component.scss']
 })
 export class PetSearchComponent implements OnInit {
+
+  // Create a new FormGroup with the FormBuilder Service
+  searchForm = this.fb.group({
+    petId: [''],
+    petType: [''],
+    location: [''],
+    searchDistance: [''],
+    lostCheck: [''],
+    foundStrayCheck: [''],
+    petGender: [''],
+    dateRange: [''],
+    sortBy: ['']
+  });
+
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() { }
+
+}
+
+
+/* BELOW WILL WORK WITH TEMPLATE DRIVE FORM. WILL BE COMMENTED OUT WHILE REACTIVE FORM IS BEING MADE 8/25/2019
+export class PetSearchComponent implements OnInit {
   pets: Pet[];
   submitted = false;
   errorMsg = '';
 
-  // petType select box array
-  petTypes = ['All Species', 'Cat', 'Dog', 'Other'];
-
-  // searchDistance select box array
-  searchDistances = ['10 miles', '25 miles', '50 miles', '100 miles'];
-
-  // dateRange select box array
-  dateRanges = [
-    'Last Day',
-    'Last 3 Days',
-    'Last Week',
-    'Last 2 Weeks',
-    'Last Month',
-    'Any Time'
-  ];
-
-  // sortBy select box array
-  sortBySelections = ['Most Recent', 'Distance'];
-
-  // new Pet property
-  petSearch = new Pet(
-    '',
-    this.petTypes[0],
-    '',
-    this.searchDistances[0],
-    true,
-    true,
-    false,
-    '',
-    this.dateRanges[0],
-    this.sortBySelections[0]
-  );
-
-  constructor(private petService: PetService) { }
-
-  onSubmit() {
-    this.submitted = true;
-    this.petService.getPetSearch(this.petSearch)
-      .subscribe(
-        data => console.log('Success!', data),
-        error => this.errorMsg = error.statusText
-      );
+  /* BELOW WILL WORK WITH TEMPLATE DRIVE FORM. WILL BE COMMENTED OUT WHILE REACTIVE FORM IS BEING MADE 8/25/2019
+    // petType select box array
+    petTypes = ['All Species', 'Cat', 'Dog', 'Other'];
+  
+    // searchDistance select box array
+    searchDistances = ['10 miles', '25 miles', '50 miles', '100 miles'];
+  
+    // dateRange select box array
+    dateRanges = [
+      'Last Day',
+      'Last 3 Days',
+      'Last Week',
+      'Last 2 Weeks',
+      'Last Month',
+      'Any Time'
+    ];
+  
+    // sortBy select box array
+    sortBySelections = ['Most Recent', 'Distance'];
+  
+    // new Pet property
+    petSearch = new Pet(
+      '',
+      this.petTypes[0],
+      '',
+      this.searchDistances[0],
+      true,
+      true,
+      false,
+      '',
+      this.dateRanges[0],
+      this.sortBySelections[0]
+    );
+  
+    constructor(private petService: PetService) { }
+  
+    onSubmit() {
+      this.submitted = true;
+      this.petService.getPetSearch(this.petSearch)
+        .subscribe(
+          data => console.log('Success!', data),
+          error => this.errorMsg = error.statusText
+        );
+    }
+  
+    ngOnInit() {
+      this.petService.getPets().subscribe(pets => {
+        this.pets = pets;
+      });
+    }
   }
-
-  ngOnInit() {
-    this.petService.getPets().subscribe(pets => {
-      this.pets = pets;
-    });
-  }
-}
+  */
 
   // Below adds petSearch data to DOM using data stored in local json file
   // onSubmit() {
@@ -72,3 +100,5 @@ export class PetSearchComponent implements OnInit {
   // }
   // ngOnInit() { } // Remove after setting up petService url to post data
   // }
+
+}
