84b3fcdef339ce82f0f3df37697a0682f148ae7b
Gerald Scott Kirkland <skirkland8@gmail.com>
2019-09-05 14:58:19 -0400

    Configured routing for pet-search and pet-report components, and Home and Report Lost Pet navbar links. Unfinished links will redirect to pet-search component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,12 @@ import { ReportPetComponent } from './components/report-pet/report-pet.component
 
 const routes: Routes = [
   { path: 'search', component: PetSearchComponent },
-  { path: 'report', component: ReportPetComponent }
+  { path: 'report', component: ReportPetComponent },
+  {
+    path: '',
+    redirectTo: '/search',
+    pathMatch: 'full'
+  }
 ];
 
 @NgModule({
